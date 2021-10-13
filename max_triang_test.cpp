70d900f2e39b027ded1b181dfc45106a829d92a2
Роман <jellybe@yandex.ru>
2021-10-13 22:56:24 +0300

    Исправления в соответствии с valgrind.

--- max_triang_test.cpp
+++ max_triang_test.cpp
@@ -16,7 +16,7 @@ TEST(MTR_TEST, VALCHECK) {
     const int **pans = max_triang(pX, pY, n);
     EXPECT_TRUE(pans != nullptr);
     for (int i = 0; i < 3; i++) EXPECT_EQ(ans[i], pans[i][0]);
-    free(pX); free(pY); free(pans);
+    free(pX); free(pY); freep((void**)pans, 3);
 }
 
 TEST(MTR_TEST, BADSTATE_CHECK) {
@@ -26,7 +26,7 @@ TEST(MTR_TEST, BADSTATE_CHECK) {
     const float **pX = arr_to_parr(X, n), **pY = arr_to_parr(Y, n);
     const int **pans = max_triang(pX, pY, n);
     EXPECT_TRUE(pans == nullptr);
-    free(pans); free(pX); free(pY);
+    free(pX); free(pY);
 }
 
 int main(int argc, char** argv) {
